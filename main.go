a9208ed5a2665d5a73e54b22d7c47c15c9fac08e
Felix Lausch <felix.lausch@justin-legal.com>
2025-05-26 18:13:14 +0200

    add simple request handlers with http response

--- main.go
+++ main.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"log"
 	"net"
+	"os"
 	"strings"
 )
 
@@ -19,8 +20,8 @@ func main() {
 	defer listener.Close()
 	log.Println("Listening on port:", PORT)
 
-	// AddRequestHandlers()
 	router := NewRouter()
+	AddRequestHandlers(router)
 
 	// Accept connections indefinitely
 	for {
@@ -235,7 +236,26 @@ const (
 // )
 
 func AddRequestHandlers(router *Router) {
-	router.RequestHandlers[Route{"/", GET}] = func(req Request) *Response {
-		return NewResponse(200, nil, "Hello")
+	indexHandler := func(req Request) *Response {
+		return ServeStaticHtml("static/index.html")
 	}
+
+	aboutHandler := func(req Request) *Response {
+		return ServeStaticHtml("static/about.html")
+	}
+
+	router.RequestHandlers[Route{"/", GET}] = indexHandler
+	router.RequestHandlers[Route{"/about", GET}] = aboutHandler
+}
+
+func ServeStaticHtml(file string) *Response {
+	content, err := os.ReadFile(file)
+	if err != nil {
+		return NewResponse(INTERNAL_SERVER_ERROR, nil, "Internal Server Error")
+	}
+
+	headers := make(map[string]string)
+	headers["Content-Type"] = "text/html"
+
+	return NewResponse(200, headers, string(content))
 }
