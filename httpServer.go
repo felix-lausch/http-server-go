2a656d3fff3a1181a418434f0e628a00f9184a24
felix lausch <baumthefeljoy@live.de>
2025-06-10 17:47:11 +0200

    - refactor parse request - try adding connection: keep alive handling

--- httpServer.go
+++ httpServer.go
@@ -1,12 +1,14 @@
 package main
 
 import (
+	"bufio"
 	"crypto/tls"
-	"errors"
 	"fmt"
+	"io"
 	"log"
 	"net"
 	"strings"
+	"time"
 )
 
 type HttpServer struct {
@@ -55,72 +57,121 @@ func (s *HttpServer) Start() {
 
 func handleConnection(conn net.Conn, router *TrieRouter) {
 	defer conn.Close()
-	var res *Response
-
 	log.Println("handling Connection:", conn.LocalAddr())
 
-	req, err := ParseRequest(conn)
-	if err != nil {
-		errMsg := fmt.Sprintf("Error parsing request: %s", err)
+	// Set a reasonable deadline for the entire connection
+	conn.SetDeadline(time.Now().Add(30 * time.Second))
 
-		log.Print(errMsg)
-		res = NewResponse(400, nil, errMsg)
-	} else {
+	for {
+		req, err := ParseRequest(conn)
+		if err != nil {
+			if err == io.EOF {
+				log.Println("Client closed connection")
+			} else if netErr, ok := err.(net.Error); ok && netErr.Timeout() {
+				log.Println("Connection timeout")
+			} else {
+				log.Printf("Error parsing request: %v", err)
+				// Send 400 Bad Request if parsing failed
+				res := NewResponse(400, nil, "Bad Request")
+				conn.Write([]byte(res.String()))
+			}
+			return
+		}
+
 		log.Println(req)
-		res = router.HandleRequest(req)
-	}
+		res := router.HandleRequest(req)
 
-	// Send HTTP response
-	_, err = conn.Write([]byte(res.String()))
-	if err != nil {
-		log.Printf("Error writing response: %v", err)
+		// Set Connection header based on client request
+		if req.Headers["Connection"] == "keep-alive" {
+			res.Headers["Connection"] = "keep-alive"
+			res.Headers["Keep-Alive"] = "timeout=30"
+		}
+
+		// Send HTTP response
+		_, err = conn.Write([]byte(res.String()))
+		if err != nil {
+			log.Printf("Error writing response: %v", err)
+		}
+
+		// Break loop if client requested connection close
+		if req.Headers["Connection"] == "close" {
+			return
+		}
 	}
 }
 
-func ParseRequest(conn net.Conn) (Request, error) {
-	buffer := make([]byte, 4096) //TODO: make reading smarter? what if body is larger than this?
-	n, err := conn.Read(buffer)
+func ParseRequest(conn net.Conn) (*Request, error) {
+	reader := bufio.NewReader(conn)
+
+	startLine, err := reader.ReadString('\n')
 	if err != nil {
-		return Request{}, err
+		return nil, err
 	}
 
-	content := string(buffer[:n])
-	log.Printf("Received:\n%s", content)
-
-	splitContent := strings.Split(content, "\r\n\r\n")
-	if len(splitContent) != 2 {
-		return Request{}, errors.New("request isn't http formatted")
-	}
-
-	requestInfo := splitContent[0]
-	headerLines := strings.Split(requestInfo, "\r\n")
-
-	startLineSplit := strings.Split(headerLines[0], " ")
-	if len(startLineSplit) != 3 {
-		return Request{}, fmt.Errorf("http start line is not correctly formatted: %v", startLineSplit)
+	startLineSplit := strings.Split(strings.TrimSpace(startLine), " ")
+	if len(startLineSplit) < 3 {
+		return nil, fmt.Errorf("http start line is not correctly formatted: %v", startLine)
 	}
 
 	method, err := ParseHttpMethod(startLineSplit[0])
 	if err != nil {
-		return Request{}, err
+		return nil, err
 	}
 
 	path, queryParams := ParseRequestTarget(startLineSplit[1])
 
-	headers := make(map[string]string, len(headerLines[1:]))
-	for _, headerLine := range headerLines[1:] {
-		splitHeaderLine := strings.Split(headerLine, ": ")
-		headers[splitHeaderLine[0]] = splitHeaderLine[1]
-	}
-
-	return Request{
+	req := &Request{
 		Method:      method,
 		Path:        path,
-		HttpVersion: startLineSplit[2],
+		Headers:     make(map[string]string),
 		QueryParams: queryParams,
-		Headers:     headers,
-		Body:        splitContent[1],
-	}, nil
+		HttpVersion: startLineSplit[2],
+	}
+
+	for {
+		headerLine, err := reader.ReadString('\n')
+		if err != nil {
+			return nil, fmt.Errorf("error reading header line: %v", err)
+		}
+
+		if headerLine == "\r\n" {
+			break
+		}
+
+		headerLineSplit := strings.Split(headerLine, ": ")
+		req.Headers[headerLineSplit[0]] = headerLineSplit[1]
+	}
+
+	// Handle body if present
+	// if contentLenStr := req.Headers["Content-Length"]; contentLenStr != "" {
+	// contentLength, err := strconv.Atoi(contentLenStr)
+	// if err != nil {
+	// 	return nil, fmt.Errorf("invalid Content-Length: %v", err)
+	// }
+
+	// buffered, err := io.ReadAll(io.LimitReader(reader, 2048))
+	// req.Body = string(buffered)
+
+	// buffer := make([]byte, 2048)
+
+	// n, err := reader.Read(buffer)
+	// if err != nil {
+	// 	return nil, err
+	// }
+
+	// log.Println("N:", n)
+
+	// req.Body = string(buffer[:n])
+
+	// _, err = io.ReadFull(reader, body)
+	// if err != nil {
+	// 	return nil, err
+	// }
+
+	// req.Body = string(body)
+	// }
+
+	return req, nil
 }
 
 func ParseRequestTarget(requestTarget string) (path string, queryArgs map[string][]string) {
